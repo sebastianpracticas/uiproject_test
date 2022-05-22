a27ec0bc20b56fd3fd01f3c838475c3737061510
jsd025 <j.sebastian.practicas@gmail.com>
2022-05-22 20:52:28 +0200

    Primeros cambios dashboard

--- resources/views/vistas/dashboard.blade.php
+++ resources/views/vistas/dashboard.blade.php
@@ -4,12 +4,23 @@
 <!-- Inicio de la seccion content -->
 @section('content')
 
-
-
-
-    Estas en dashboard
-
-
-
+<div class="dashboard-principal row">
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 dashboard_menu">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dashboard-button">
+                Mi Cuenta
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dashboard-button">
+                Mensajes
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dashboard-button">
+                Pedidos
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9 dashboard_content">
+        <p>TEST</p>
+    </div>
+</div>
 
 @endsection('content')
