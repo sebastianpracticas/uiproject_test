55793c63a92b84390c83632a583fbf46a11daf17
jsd025 <j.sebastian.practicas@gmail.com>
2022-05-23 21:55:30 +0200

    Solucionando detalles visuales home

--- resources/views/vistas/home.blade.php
+++ resources/views/vistas/home.blade.php
@@ -17,17 +17,10 @@
 <!--Nosotros-->
 <div class="container anim-aparecer">
     <div class="Nosotros row">
-<<<<<<< HEAD
         <div class="imagen-nosotros hideme col-xs-12 col-sm-6 col-md-6 col-lg-6 align-self-center">
             <img  class="d-block" src="img/Nosotros.png" alt="imagen de nuestro equipo hablando">
         </div> 
         <div class="Nosotros-texto hideme col-xs-12 col-sm-6 col-md-6 col-lg-6 align-self-center">
-=======
-        <div class="imagen-nosotros col-xs-12 col-sm-6 col-md-6 col-lg-6 align-self-center">
-            <img class="d-block" src="img/Nosotros.png" alt="imagen de nuestro equipo hablando">
-        </div>
-        <div class="Nosotros-texto  col-xs-12 col-sm-6  col-md-6 col-lg-6 align-self-center">
->>>>>>> 05133d4d7942b59a90bc2139bf04e8c36ae538e7
             <div>
                 <h1 class="h1">¿Quiénes somos?</h1>
             </div>
@@ -116,7 +109,6 @@
 
 <div class="container">
     <div class="row mar-bottom-70-px" style="margin-bottom: 70px">
-<<<<<<< HEAD
         <div class="col-12 hideme">
             <h1 class="h1 align-center">¿Por qué elegirnos?</h1> 
         </div>
@@ -130,21 +122,6 @@
         </div>
         <div class="col-12 col-md-4 align-center hideme">
             <h1>Especialistas</h1> 
-=======
-        <div class="col-12">
-            <h1 class="h1 align-center">¿Por qué elegirnos?</h1>
-        </div>
-        <div class="col-12 col-md-4 align-center">
-            <h1>Trato personalizado</h1>
-            <p>Atendemos a tus consultas de manera rápida. Incluimos el mantenimiento dentro de nuestro servicio.</p>
-        </div>
-        <div class="col-12 col-md-4 align-center rel-top-70-px" style="position: relative; top: 70px;">
-            <h1>Manejo de tecnologías</h1>
-            <p>Hacemos uso de multitud de tecnologias para la creación de tu web, todo pasa por un proceso ordenado y el resultado es un servicio eficaz y profesional.</p>
-        </div>
-        <div class="col-12 col-md-4 align-center">
-            <h1>Especialistas</h1>
->>>>>>> 05133d4d7942b59a90bc2139bf04e8c36ae538e7
             <p>Somos especialistas en diseño y creacion de app y sitios web. Contamos con un equipo multidisciplicar par el exito de tu proyecto.</p>
         </div>
     </div>
