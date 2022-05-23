716d924196a5c325f4744366784165ff2096a806
Bryan <bryan@bryan.com>
2022-05-23 19:36:00 +0200

    Slider

--- resources/views/vistas/home.blade.php
+++ resources/views/vistas/home.blade.php
@@ -70,6 +70,22 @@
     </div>
 </div>
 
+
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/img/Portafolio.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/img/Portalfolio3.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/img/Portafolio1.png" alt="Third slide">
+    </div>
+  </div>
+</div>
+
 <!--Razones para elegirnos-->
 
 <div class="container">
