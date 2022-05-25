97d6e0fb5090a01bfb2c0913fb6cc7c67f01248a
jsd025 <j.sebastian.practicas@gmail.com>
2022-05-25 02:29:48 +0200

    Añadiendo funcion de insercion de mensajes a MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    public function newMessage() {
-        return false;
+    public function newMessage(Request $request) {
+        $userId = Auth::user()->id;
+        $userRol = Auth::user()->role;
+        $cuentaId = json_decode(json_encode(DB::table('cuentas')->where('id_usuarios',$userId)->get("id")->first()), true)["id"];
+
+        $mensaje = $request->mensaje;
+
+        if ($userRol == "user") {
+            $respuesta = 0;
+        } else {
+            $userId = $request->userId;
+            $respuesta = 1;
+        }
+
+        DB::table('mensajes')->insert([
+            'id_cuenta' => $userId,
+            'mensaje' => $mensaje,
+            'leido' => 0,
+            'archivado' => 0,
+            'es_respuesta' => $respuesta,
+            'fecha_hora' => strtotime(time()+7200)
+        ]);
     }
 }
