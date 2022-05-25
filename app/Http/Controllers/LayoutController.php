c0d084ff2050731201c54cf7ff49a7de88a0612f
jsd025 <j.sebastian.practicas@gmail.com>
2022-05-25 02:36:58 +0200

    Añadiendo funcion de insert y update a LayoutController

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -6,11 +6,30 @@ use Illuminate\Http\Request;
 
 class LayoutController extends Controller
 {
-    public function newLayout() {
-        return false;
+    public function newLayout(Request $request) {
+        $precio = $request->precio;
+        $descripcion = $request->descripcion;
+        $url = $request->url;
+
+        DB::table('plantillas')->insert([
+            'precio' => $precio,
+            'descripcion' => $descripcion,
+            'url' => $url
+        ]);
+        return true;
     }
 
-    public function updateLayout() {
-        return false;
+    public function updateLayout(Request $request) {
+        $id = $request->id;
+        $precio = $request->precio;
+        $descripcion = $request->descripcion;
+        $url = $request->url;
+
+        DB::table('plantillas')->where('id', $id)->update([
+            'precio' => $precio,
+            'descripcion' => $descripcion,
+            'url' => $url
+        ]);
+        return true;
     }
 }
